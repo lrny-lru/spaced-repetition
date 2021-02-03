b58507e344cd546d3b3c5bb210da29d6b9f14998
lrny-lru <60946728+lrny-lru@users.noreply.github.com>
2021-02-03 12:29:13 -0500

    fix auth

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports= {
-  API_ENDPOINT: "https://amadan.herokuapp.com",
+  API_ENDPOINT: "https://amadan.herokuapp.com/api",
   //API_ENDPOINT: "http://localhost:8000/api",
   TOKEN_KEY: "blogful-client-auth-token",
 };
